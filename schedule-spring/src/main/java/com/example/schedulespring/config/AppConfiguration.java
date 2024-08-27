7ef3543f0086394ebbfd3dea07c5843ca7de6e72
kyb <kyb-0215@naver.com>
2024-08-27 22:59:46 +0900

    안쓰는 코드 지우기

--- schedule-spring/src/main/java/com/example/schedulespring/config/AppConfiguration.java
+++ schedule-spring/src/main/java/com/example/schedulespring/config/AppConfiguration.java
@@ -1,27 +1,11 @@
 package com.example.schedulespring.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfiguration {
 
-    @Value("${jwt.public.key}")
-    RSAPublicKey key;
-
-    @Value("${jwt.private.key}")
-    RSAPrivateKey priv;
-
-    @Value("${aws.access.key}")
-    String accessKey;
-
-    @Value("${aws.secret.key}")
-    String secretKey;
 
 }
