dd31c5b9acc6971534d154fd83e2d17fa0e9c3df
kyb <kyb-0215@naver.com>
2024-09-01 20:44:52 +0900

    Security 로직 추가

--- schedule-spring/src/main/java/com/example/schedulespring/config/AppConfiguration.java
+++ schedule-spring/src/main/java/com/example/schedulespring/config/AppConfiguration.java
@@ -1,11 +1,31 @@
 package com.example.schedulespring.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfiguration {
 
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .formLogin(login -> login.loginPage("/member/login"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
