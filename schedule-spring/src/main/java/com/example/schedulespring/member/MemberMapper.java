9fc9bc69313010ae4cdfaf0d4a5597b7059615fc
kyb <kyb-0215@naver.com>
2024-09-03 20:17:42 +0900

    home 설정

--- schedule-spring/src/main/java/com/example/schedulespring/member/MemberMapper.java
+++ schedule-spring/src/main/java/com/example/schedulespring/member/MemberMapper.java
@@ -3,5 +3,5 @@ package com.example.schedulespring.member;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class MemberMapper {
+public interface MemberMapper {
 }
--- schedule-spring/src/main/java/com/example/schedulespring/member/MemberService.java
+++ schedule-spring/src/main/java/com/example/schedulespring/member/MemberService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberMapper memberMapper;
 
