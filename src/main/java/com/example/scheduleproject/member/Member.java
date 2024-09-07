3b69f2fb8ddcc0abe1019feb6b46a3afc6c5e31e
kyb <kyb-0215@naver.com>
2024-09-08 01:47:54 +0900

    inserted 추가

--- src/main/java/com/example/scheduleproject/member/Member.java
+++ src/main/java/com/example/scheduleproject/member/Member.java
@@ -2,11 +2,13 @@ package com.example.scheduleproject.member;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Member {
     private int id;
     private String email;
     private String password;
     private String nickName;
-
+    private LocalDateTime inserted;
 }
