202a0047aa6c0181a775f1a36877b2ebcaa6b11f
kyb <kyb-0215@naver.com>
2024-09-07 14:06:09 +0900

    회원가입 설명

--- src/main/java/com/example/scheduleproject/member/MemberController.java
+++ src/main/java/com/example/scheduleproject/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.scheduleproject.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     private final MemberService memberService;
 
+    @Description("회원가입")
     @PostMapping("join")
     public String join(Member member) {
         memberService.join(member);
