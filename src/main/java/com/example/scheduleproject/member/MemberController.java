874434b20fee0c9cca44f2e9890f88d7db096a83
kyb <kyb-0215@naver.com>
2024-09-09 13:44:59 +0900

    회원가입, 로그인 페이지 보여주기

--- src/main/java/com/example/scheduleproject/member/MemberController.java
+++ src/main/java/com/example/scheduleproject/member/MemberController.java
@@ -3,6 +3,7 @@ package com.example.scheduleproject.member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     private final MemberService memberService;
 
+    @Description("회원가입 페이지 Form 형식 보기")
+    @GetMapping("join")
+    public void joinForm() {}
+
     @Description("회원가입")
     @PostMapping("join")
     public String join(Member member) {
         memberService.join(member);
-        return "redirect:/login";
+        return "redirect:/member/login";
     }
+
+    @Description("로그인 페이지 Form 형식 보기")
+    @GetMapping("login")
+    public String loginForm() {
+        return "member/login";
+    }
+
 }
