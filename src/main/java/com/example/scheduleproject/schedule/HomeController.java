368ba783b2a76084b5b15651ea6ca1696958c3c8
kyb <kyb-0215@naver.com>
2024-09-14 22:03:47 +0900

    캘린더 설명

--- src/main/java/com/example/scheduleproject/schedule/HomeController.java
+++ src/main/java/com/example/scheduleproject/schedule/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.scheduleproject.schedule;
 
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Description("캘린더 보여주기")
     @GetMapping("/")
     public String home() {
-        System.out.println(" homeeeee ");
         return "main/home";
     }
 }
