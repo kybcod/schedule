a109ed19af5c67a00b9737d0cb6a711d4396e699
kyb <kyb-0215@naver.com>
2024-09-16 19:25:01 +0900

    @RequiredArgsConstructor 추가

--- src/main/java/com/example/scheduleproject/schedule/HomeController.java
+++ src/main/java/com/example/scheduleproject/schedule/HomeController.java
@@ -1,20 +1,17 @@
 package com.example.scheduleproject.schedule;
 
 import com.example.scheduleproject.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
     private final MemberService memberService;
-    private ScheduleService scheduleService;
-
-
-    public HomeController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    private final ScheduleService scheduleService;
 
     @Description("캘린더 보여주기")
     @GetMapping("/")
