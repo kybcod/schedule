1b85861044710f1571c58ba3d402328148530de0
kyb <kyb-0215@naver.com>
2024-09-24 14:38:57 +0900

    추가 쿼리 작성

--- sql/test.sql
+++ sql/test.sql
@@ -1,8 +1,16 @@
 USE schedule;
+
 CREATE TABLE member(
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(200) NOT NULL UNIQUE ,
     password VARCHAR(200) NOT NULL ,
     nick_name VARCHAR(100) NOT NULL UNIQUE ,
+    inserted DATETIME NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE schedule(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NOT NULL ,
     inserted DATETIME NOT NULL DEFAULT NOW()
 );
